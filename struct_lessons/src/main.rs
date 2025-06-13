c41f0ae2fe782934c9ab1a6b59f6053eb1bfa605
olathedev <olapademicheal3@gmail.com>
2025-06-13 20:37:07 +0100

    tupple like structs

--- struct_lessons/src/main.rs
+++ struct_lessons/src/main.rs
@@ -5,27 +5,33 @@ struct Player {
 }
 
 
+// fn main() {
+//     let player1 = Player {
+//         username: String::from("Oganla"),
+//         streak: 0,
+//         points: 0
+//     };
+
+//     let player1_username = player1.username;
+//     println!("{}", player1_username)
+
+//     let player2 = create_player(String::from("john doe"))
+//     let player3 = create_player(String::from("salama"))
+
+
+// }
+
+
+// fn create_player(username: String) -> Player {
+//     Player {
+//         username,
+//         points: 0,
+//         streak: 0,
+//     }
+// }
+
+// Tupple like structs
 fn main() {
-    let player1 = Player {
-        username: String::from("Oganla"),
-        streak: 0,
-        points: 0
-    };
-
-    let player1_username = player1.username;
-    println!("{}", player1_username)
-
-    let player2 = create_player(String::from("john doe"))
-    let player3 = create_player(String::from("salama"))
-
-
-}
-
-
-fn create_player(username: String) -> Player {
-    Player {
-        username,
-        points: 0,
-        streak: 0,
-    }
+    struct Color(i32, i32, i32)
+    struct Location(i64, i64, i64)
 }
