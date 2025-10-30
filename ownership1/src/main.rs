7c6a7259ab0b5f418e5c7c6584e2e6f47c6749c7
olathedev <olapademicheal3@gmail.com>
2025-10-30 15:55:54 +0100

    ownership lessons

--- ownership1/src/main.rs
+++ ownership1/src/main.rs
@@ -1,3 +1,36 @@
+// note references are immutable and we cant borrow the value as mutable in here
+// fn main() {
+//     let s1 = String::from("Heyy");
+//     let len = calculate_length(&s1);
+//     println!("the length of {} is {}", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     let length = s.len();
+//     length
+// }
+
+// situation where you want to borrow value as mutable
+// fix: use a mutabele borrow
+// note: you can only have one mutable reference for a particular data in a scope
+// fn main() {
+//     let mut s1 = String::from("Heyy");
+//     change_string(&mut s1);
+// }
+
+// fn change_string(param: &mut String) {
+//     param.push_str(", yoo");
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Heyy");
+
+    let s1 = &s;
+    let s2 = &s;
+    
+    println!("{s}");
+
+    let s3 = &mut s;
 }
+
+// Dangling reference: a varibale that points to invalid data
